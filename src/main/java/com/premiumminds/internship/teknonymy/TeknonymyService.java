cf1dd592e313682ca5f8317b0deb41bc942c7237
carlota <carlota.domingos@tecnico.ulisboa.pt>
2024-06-27 17:05:56 +0100

    soultion and 3 added tests

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -1,16 +1,69 @@
 package com.premiumminds.internship.teknonymy;
 
 import com.premiumminds.internship.teknonymy.Person;
+import java.util.*;
 
 class TeknonymyService implements ITeknonymyService {
 
+ 
   /**
-   * Method to get a Person Teknonymy Name
+   * Method to get the string equivalent to the solution of the problem 
+   * 
+   * @param Person descendant
+   * @param Person person
+   * @param int level
+   * @return String which is the Teknonymy Name
+  */
+  private String getTeknonymyName(Person descendant, Person person, int level) {
+    String init;
+    if (person.sex().equals('M')){
+      init = "father of ";
+    } else {
+      init = "mother of ";
+    }
+    if (level > 2) {
+      return "great-".repeat(level-3) + "grand" + init + descendant.name();
+    } else {
+      return init + descendant.name();
+    }
+  }
+
+   /**
+   * Method to get a Person Teknonymy Name trough a breadth-first search
    * 
    * @param Person person
    * @return String which is the Teknonymy Name 
    */
   public String getTeknonymy(Person person) {
-    throw new RuntimeException("Not Implemented Yet");
-  };
+    int level = 0;
+    int levelsize;
+    boolean newlevel = true;
+    Person descendant = null;
+
+    Queue<Person> queue = new LinkedList<>();
+    queue.add(person);
+    
+    while(!queue.isEmpty()){
+      levelsize = queue.size();
+      newlevel = true;
+
+      for(int i = 0; i < levelsize; i++){
+        Person current = queue.poll();
+        if(current.children() != null){
+          for(Person child : current.children()){
+            queue.add(child);
+            if(newlevel || child.dateOfBirth().isBefore(descendant.dateOfBirth())){
+              newlevel = false;
+              descendant = child;
+            }
+          }
+        }
+      }
+      level++;
+    }
+
+    if (descendant == null) 
+      return "";
+    return getTeknonymyName(descendant, person, level);
+  }
 }
--- src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
+++ src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
@@ -47,4 +47,74 @@ public class TeknonymyServiceTest {
     String expected = "father of Holy";
     assertEquals(result, expected);
   }
+
+  @Test
+  public void PersonTwoChildrenTest() {
+    Person person = new Person(
+        "John",
+        'M',
+        new Person[]{ 
+          new Person("Holy",'F', null, LocalDateTime.of(1045, 1, 1, 0, 0)),
+          new Person("Mary",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0))
+        },
+        LocalDateTime.of(1046, 1, 1, 0, 0));
+    String result = new TeknonymyService().getTeknonymy(person);
+    String expected = "father of Holy";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void test4() {
+    Person faby = new Person("Faby",'F', null, LocalDateTime.of(1045, 1, 1, 0, 0));
+    Person jane = new Person("Jane",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0));
+    Person diana = new Person("Diana",'F', null, LocalDateTime.of(1047, 1, 1, 0, 0));
+    Person carlos = new Person("Carlos",'M', new Person[]{ faby}, LocalDateTime.of(1048, 1, 1, 0, 0));
+    Person marie = new Person("Marie",'F', new Person[]{ jane, diana}, LocalDateTime.of(1049, 1, 1, 0, 0));
+    Person liam = new Person("Liam",'M', new Person[]{ carlos}, LocalDateTime.of(1050, 1, 1, 0, 0));
+    Person luke = new Person("Luke",'M', null, LocalDateTime.of(1051, 1, 1, 0, 0));
+    Person charles = new Person("Charles",'M',null, LocalDateTime.of(1052, 1, 1, 0, 0));
+    Person joanne = new Person("Joanne",'F', null, LocalDateTime.of(1053, 1, 1, 0, 0));
+    Person lois = new Person("Lois",'F', new Person[]{ liam, luke}, LocalDateTime.of(1054, 1, 1, 0, 0));
+    Person john = new Person("John",'M', new Person[]{ marie, joanne}, LocalDateTime.of(1055, 1, 1, 0, 0));
+    Person anne = new Person("Anne",'F', new Person[]{ charles}, LocalDateTime.of(1056, 1, 1, 0, 0));
+    Person person = new Person("Peter",'M', new Person[]{ john, lois, anne}, LocalDateTime.of(1057, 1, 1, 0, 0));
+    String result = new TeknonymyService().getTeknonymy(person);
+    String expected = "great-great-grandfather of Faby";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void Test5(){
+    Person jane = new Person("Jane",'F', null, LocalDateTime.of(1046, 1, 1, 0, 0));
+    Person diana = new Person("Diana",'F', null, LocalDateTime.of(1047, 1, 1, 0, 0));
+    Person marie = new Person("Marie",'F', new Person[]{ jane, diana}, LocalDateTime.of(1049, 1, 1, 0, 0));
+    
+    Person charles = new Person("Charles",'M',null, LocalDateTime.of(1070, 1, 1, 0, 0));
+    Person joanne = new Person("Joanne",'F', null, LocalDateTime.of(1053, 1, 1, 0, 0));
+   
+    Person john = new Person("John",'M', new Person[]{ marie, joanne}, LocalDateTime.of(1055, 1, 1, 0, 0));
+    Person anne = new Person("Anne",'F', new Person[]{ charles}, LocalDateTime.of(1056, 1, 1, 0, 0));
+    Person person = new Person("Joanna",'F', new Person[]{ john, anne}, LocalDateTime.of(1057, 1, 1, 0, 0));
+    String result = new TeknonymyService().getTeknonymy(person);
+    String expected = "great-grandmother of Jane";
+    assertEquals(result, expected);
+  }
+
+  @Test
+  public void Test6(){
+    Person marie = new Person("Marie",'F', null, LocalDateTime.of(1049, 1, 1, 0, 0));
+    Person liam = new Person("Liam",'M', null, LocalDateTime.of(1050, 1, 1, 0, 0));
+    Person luke = new Person("Luke",'M', null, LocalDateTime.of(1051, 1, 1, 0, 0));
+    Person charles = new Person("Charles",'M',null, LocalDateTime.of(1052, 1, 1, 0, 0));
+    Person joanne = new Person("Joanne",'F', null, LocalDateTime.of(1053, 1, 1, 0, 0));
+    Person lois = new Person("Lois",'F', new Person[]{ liam, luke}, LocalDateTime.of(1054, 1, 1, 0, 0));
+    Person john = new Person("John",'M', new Person[]{ marie, joanne}, LocalDateTime.of(1055, 1, 1, 0, 0));
+    Person anne = new Person("Anne",'F', new Person[]{ charles}, LocalDateTime.of(1056, 1, 1, 0, 0));
+    Person person = new Person("Peter",'M', new Person[]{ john, lois, anne}, LocalDateTime.of(1057, 1, 1, 0, 0));
+    String result = new TeknonymyService().getTeknonymy(person);
+    String expected = "grandfather of Marie";
+    assertEquals(result, expected);
+  }
+
+
 }
